bb3e1ca72fcb7089626bdfb16c5ceca4f0faa80b
Vineeth Shankar <shankar.vineeth@gmail.com>
2014-11-27 09:40:28 -0500

    CCS implemented

--- pagerank_sparse.c
+++ pagerank_sparse.c
@@ -6,11 +6,11 @@
 // damp2 - (1-d)/N
 // prnew <- R
 // a <- M
-// **************Compressed row storage technique**************//
+// **************Compressed column storage technique**************//
 // All non zero elements will be stored as val vector
-// Column indices of these elements are in colind vector
-// Indices of those elements which start a new row
-// are in rowptr vector
+// Row indices of these elements are in rowind vector
+// Indices of those elements which start a new column
+// are in colptr vector
 
 main()
 {
@@ -19,7 +19,7 @@ main()
 	//double a[4][4] = {{0}};
 	double val[9] = {0}; //there are 9 edges or 9 non zero elements
 	int rowind[9] ={0};
-	int colptr[10] = {1}; //missing  values will be initialized to zero?
+	int colptr[5] = {1}; //missing  values will be initialized to zero? no.of cols+1
 	double prold[4] = {0.25,0.25,0.25,0.25};
 	double prnew[4] = {0.0,0.0,0.0,0.0};
 	double diff[4] = {0.0,0.0,0.0,0.0};
@@ -47,49 +47,58 @@ main()
 	sum[j] = localsum; //for the last column
 	colptr[j+1]= 9; //number of non zeros in the matrix
 	fclose(fp);
-	for(i = 0; i<4; i++) {
-		printf("\n");
-		for(j = 0; j<4; j++) {
-			printf("%f\t",a[i][j]);
-		}
-	}
-	printf("\n");	
+//	for(i = 0; i<4; i++) {
+//		printf("\n");
+//		for(j = 0; j<4; j++) {
+//			printf("%f\t",a[i][j]);
+//		}
+//	}
+
+	printf("this is sum array\n");	
 	// sum array
 	for(j = 0; j< 4; j++) {
 		printf("%d\t", sum[j]);
 	}
 	printf("\n");
-	for(j=0;j<4;j++) {
-		for(i=0;i<4;i++) {
-			a[i][j] = a[i][j]/sum[j];
-		}
+	printf("this is the rowind array\n");
+	for(i=0;i<9;i++) {
+		printf("%d\t", rowind[i]);
 	}
+        printf("this is the colptr array\n");
+        for(i=0;i<5;i++) {
+		printf("%d\t", colptr[i]);
+	}
+//	for(j=0;j<4;j++) {
+//		for(i=0;i<4;i++) {
+//			a[i][j] = a[i][j]/sum[j];
+//		}
+//	}
         printf("printing final array\n");  
-	for(i = 0; i<4; i++) {
-		printf("\n");
-		for(j=0; j<4; j++) {
-			printf("%f\t", a[i][j]);
-		}
+//	for(i = 0; i<4; i++) {
+//		printf("\n");
+//		for(j=0; j<4; j++) {
+//			printf("%f\t", a[i][j]);
+//		}
 	}
-	printf("\nComputing page rank...\n");
+//	printf("\nComputing page rank...\n");
 // Power iteration
-        do {
-		norm = 0.0;
-		for(i = 0; i<4; i++) {
-			for(j=0; j<4; j++) {
-				prnew[i] += a[i][j]*prold[j];
-			}
-			prnew[i] = prnew[i]*damp1[i]+damp2[i];
-         	}
-		//copying
-		for(i=0; i<4;i++) {
-			diff[i] = prnew[i] - prold[i];
-			norm += diff[i]; //l1 norm || prnew-prold ||
-			prold[i] = prnew[i];
-		}
-	}while(abs(norm)>err);
+//        do {
+//		norm = 0.0;
+//		for(i = 0; i<4; i++) {
+//			for(j=0; j<4; j++) {
+//				prnew[i] += a[i][j]*prold[j];
+//			}
+//			prnew[i] = prnew[i]*damp1[i]+damp2[i];
+  //       	}
+//		//copying
+//		for(i=0; i<4;i++) {
+//			diff[i] = prnew[i] - prold[i];
+//			norm += diff[i]; //l1 norm || prnew-prold ||
+//			prold[i] = prnew[i];
+///		}
+//	}while(abs(norm)>err);
        //print pagrank vector	
-	for(j = 0; j< 4; j++) {
-		printf("%f\t",prnew[j]);
-	}
-}
+//	for(j = 0; j< 4; j++) {
+//		printf("%f\t",prnew[j]);
+//	}
+//}
